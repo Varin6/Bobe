1ffa99cf8156db7aeff9adf638bf9a4cd33638ea
Varin6 <varinator@gmail.com>
2019-01-30 00:10:58 +0000

    React Navigation

--- App_bak.js
+++ App_bak.js
@@ -14,11 +14,9 @@ import {
     Animated,
     Button,
     View,
-    ScrollView,
-    TouchableHighlight,
+    TouchableOpacity,
     TextInput,
-    Dimensions,
-    ActivityIndicator
+    Dimensions
 } from 'react-native';
 
 import { createStackNavigator, createAppContainer } from "react-navigation";
@@ -32,106 +30,7 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-
-
-class HomeScreen extends Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            isLoading: true,
-            notes: null
-
-        };
-    }
-
-    nextButtonPressed = () => {
-
-        console.log('Login: ' + 'Password: ');
-        alert('Login: ' + 'Password: ');
-
-    };
-
-
-
-    componentDidMount(): void {
-
-
-
-            fetch('http://77.68.13.121:4000/api/notes', {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            })
-            //.then((response) => response.json())
-                .then((responseJson) => {
-
-
-                    if (responseJson.status === 200) {
-
-                        this.setState({
-                            isLoading: false,
-                            notes: JSON.parse(responseJson._bodyText),
-                        })
-
-                        // alert('Title: \n\n' + JSON.parse(responseJson._bodyText)[0].note_title + ' \n\n' + 'Note: \n\n' + JSON.parse(responseJson._bodyText)[0].note_body);
-                        // console.log(responseJson);
-                        // console.log(JSON.parse(responseJson._bodyText)[0]);
-
-                        // this.setState({
-                        //     postcodeTown: responseJson.result.parliamentary_constituency,
-                        //     latitude: responseJson.result.latitude,
-                        //     longitude: responseJson.result.longitude,
-                        // });
-                    }
-                })
-                .catch((error) => {
-                    //console.error(error);
-                    alert(error);
-                });
-
-
-
-    }
-
-
-    render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.container}>
-                    <ActivityIndicator />
-                </View>
-            );
-        } else {
-
-            let allNotes = this.state.notes.map((val, key) => {
-
-                return <View key={key} style={styles.text}>
-                    <Text style={{marginBottom: 5}}>{val.note_title}</Text>
-                    <Text style={{marginBottom: 15}}>{val.note_body}</Text>
-                </View>
-
-            });
-
-            return (
-                <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                    <ScrollView style={{width: '100%'}}>
-                        <Text>Home Screen</Text>
-                        <Button style={styles.button} title={"Next"}
-                                onPress={() => this.props.navigation.navigate('Login')}/>
-                        {allNotes}
-                    </ScrollView>
-                </View>
-            );
-        }
-    }
-}
-
-
-class App extends Component<props> {
+export default class App extends Component<props> {
 
     constructor() {
         super();
@@ -180,8 +79,6 @@ class App extends Component<props> {
         alert('Login: ' + this.state.login + 'Password: ' + this.state.password);
 
     };
-
-
 
     notesButtonPressed = () => {
 
@@ -242,40 +139,8 @@ class App extends Component<props> {
                     secureTextEntry={true}
                 />
 
-
-                <TouchableHighlight
-                    style ={{
-                        height: 40,
-                        borderRadius:10,
-                        marginTop :20
-                    }}>
                 <Button style={styles.button} title={"Login"} onPress={this.buttonPressed}/>
-                </TouchableHighlight>
-
-                <TouchableHighlight
-                    style ={{
-                        height: 40,
-                        borderRadius:10,
-                        marginTop :20
-                    }}>
                 <Button style={styles.button} title={"Notes"} onPress={this.notesButtonPressed}/>
-                </TouchableHighlight>
-
-
-
-                <TouchableHighlight
-                    style ={{
-                        height: 40,
-                        width:160,
-                        borderRadius:10,
-                        marginTop :20
-                    }}>
-                    <Button
-                        style={{marginTop: 20}}
-                        title="Go back"
-                        onPress={() => this.props.navigation.goBack()}
-                    />
-                </TouchableHighlight>
 
 
 
@@ -306,7 +171,7 @@ const styles = StyleSheet.create({
         borderBottomColor: '#cccccc',
     },
     button: {
-        marginBottom: 10
+
     },
     text: {
         fontSize: 12,
@@ -314,23 +179,3 @@ const styles = StyleSheet.create({
 
 
 });
-
-
-const AppNavigator = createStackNavigator(
-
-    {
-        Home: HomeScreen,
-        Login: App
-    },
-    {
-        headerMode: "none"
-    },
-    {
-        initialRouteName: "Home"
-    }
-
-
-);
-
-export default createAppContainer(AppNavigator);
-
--- android/app/src/main/java/com/bobe/MainActivity.java
+++ android/app/src/main/java/com/bobe/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bobe;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Bobe";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
